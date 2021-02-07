356f45bf63c09860f5c77a39249dbe715ca3bd73
Stefan Genov <stefantigro@gmail.com>
2021-02-07 21:49:40 +0200

    [master] Added get for notes, some basic architecture.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const app	= require( 'event_request' )();
+const app = require('event_request')();
 
-require( './src/main/server/kernel' );
+require('./src/main/server/kernel');
+require('./src/apps/notes/controllers')
 
 // Start Listening
-app.listen( 8000, () => {
-	app.Loggur.log( 'Server started' );
+app.listen(8000, () => {
+	app.Loggur.log('Server started');
 });
