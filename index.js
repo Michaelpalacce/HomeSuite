8462e9225ab5a8e08756e3daec34ac2bdd61eada
Stefan Genov <stefantigro@gmail.com>
2021-02-14 16:14:36 +0200

    [master] Small changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const app = require('event_request')();
+const app	= require('event_request')();
 
-require('./src/main/server/kernel');
-require('./src/apps/notes/controllers')
+require( './src/main/server/kernel' );
+require( './src/apps/notes/controllers' )
 
 // Start Listening
-app.listen(8000, () => {
+app.listen( 80, () => {
 	app.Loggur.log('Server started');
 });
